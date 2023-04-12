d70dc86a0b0c632a98944006f948fcbee277c4ec
Edgardo803 <edgardo_148@hotmail.com>
2023-04-12 20:49:21 +0200

    add
    add project

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+const Error = ({children}) => {
+  return (
+    <div className="bg-red-600 text-white text-center p-3 
+    uppercase font-bold mb-3 rounded-md text-1xl">
+      {children}
+
+      </div>
+  )
+}
+
+export default Error
---
+++ src/components/Header.jsx
@@ -0,0 +1,68 @@
+/*
+
+import React from "react"
+import "./styles/header.css"
+
+function Header() {
+    return (
+        <h1 className="text - text-2xl text - center">
+            Seguimiento Pacientes Como Kokillo 
+            
+            <span className="text - text-4xl text - center">
+                Desde Header
+            </span>
+            
+           <span className=  "text - text-4xl text - center">
+                Veterinarias</span>
+        </h1>
+                
+    )
+}
+
+export default Header
+
+
+/*
+
+import React from "react"
+function Header(){
+
+
+    return(
+    
+        
+
+        <span className ="font-black text-8xl text-center md:w-2/3 text-indigo-600 mx-auto">
+            Seguimiento Pacientes {""}
+            <h1 className ="text - text-10xl text - center">Veterinaria</h1>
+          
+            Kokillo es un loki
+
+        </span>
+    )
+
+    }
+
+export default Header;
+
+*/
+
+
+function Header() {
+
+    return (
+        
+
+        <h1 className= "font-black text-5xl text-center md:w-2/3 mx-auto">
+         Seguimiento Pacientes {''}
+        <span className ="text-indigo-600"> Veterinaria</span>
+
+        </h1>
+
+        
+
+    )
+
+}
+
+export default Header;
